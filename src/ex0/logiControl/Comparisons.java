f99b0bd2b8e7239c47b2195a355f3089a0d899ce
elads <eladsez36@gmail.com>
2021-10-26 19:10:40 +0300

    hi

--- src/ex0/logiControl/Comparisons.java
+++ src/ex0/logiControl/Comparisons.java
@@ -23,18 +23,11 @@ public class Comparisons {
         return (distFromSrc / elev.getSpeed()) + elevTime; // time * speed = Length of the way
     }
 
-    public static int bestElev(OurAlgo algo, CallForElevator call) {
-        int bestElev = -1;
-        boolean upCall = call.getSrc() < call.getDest();
-        for (int i = 0; i < algo.getBuilding().numberOfElevetors(); i++) {
-
-        }
-        return bestElev;
-    }
-
     /**
-     * this function is for case 1 when there is available elevator.
-     *
+     * Given a callForElevator and a building (in OurAlgo shape)
+     * the function checks whether the elevator has not yet passed the source floor
+     * and whether the elevator is going in the same direction of the call.
+     * @return true if the index of the elevator Should take the call.
      * @return the best distance and time wise available elevator to the source.
      */
     public static int bestAvailElev(CallForElevator call, OurAlgo algo) {
@@ -64,64 +57,39 @@ public class Comparisons {
         return bestElevIndex;
     }
 
+    /**
+     * Given a callForElevator, access to OurAlgo and elevator's index i
+     * the function checks whether the elevator (i) has not yet passed the source floor,
+     * whether the elevator is going in the same direction of the call
+     * or the elevator not doing anything
+     * @return true if the index of the elevator Should take the call.
+     */
     public static boolean dirAndPass(OurAlgo algo, CallForElevator call, int i) {
+        //
         if (algo.up[i] && call.getSrc() < call.getDest() &&
                 algo.getBuilding().getElevetor(i).getPos() < call.getSrc())
             return true;
+        //
         if (algo.down[i] && call.getSrc() > call.getDest() &&
                 algo.getBuilding().getElevetor(i).getPos() > call.getSrc())
             return true;
-        if (!algo.down[i] && !algo.up[i]) // available elevator
+        // available elevator
+        if (!algo.down[i] && !algo.up[i])
             return true;
         return false;
     }
 
 
     /**
-     * Given a callForElevator and a building where all the elevators are occupied
+     * Given a callForElevator and a building (in OurAlgo shape)
      * the function checks whether the elevator has not yet passed the source floor
      * and whether the elevator is going in the same direction of the call.
-     *
      * @return true if the index of the elevator Should take the call.
      */
-    public static boolean[] sameDir(Building building, CallForElevator call) {
-        boolean[] goodElev = new boolean[building.numberOfElevetors()];
-        if (call.getDest() < call.getSrc()) {   // down case
-            for (int i = 0; i < building.numberOfElevetors(); i++) {
-                if (building.getElevetor(i).getState() == Elevator.DOWN &&
-                        building.getElevetor(i).getPos() >= call.getSrc())
-                    goodElev[i] = true;
-                else goodElev[i] = false;
-            }
-        } else { ///up case
-            for (int i = 0; i < building.numberOfElevetors(); i++) {
-                if (building.getElevetor(i).getState() == Elevator.UP &&
-                        building.getElevetor(i).getPos() <= call.getSrc())
-                    goodElev[i] = true;
-                else goodElev[i] = false;
-            }
-        }
-        return goodElev;
-    }
 
     /**
      * @return the best distance and time wise same direction elevator to the source.
      */
 
-    public static int bestSameDirElev(boolean[] goodElev, Building building, int src) {
-        int bestElevIndex = -1;
-        double bestTimeToSrc = Double.MAX_VALUE;
-        Elevator curr;
-        for (int i = 0; i < building.numberOfElevetors(); i++) {
-            if (goodElev[i]) {
-                curr = building.getElevetor(i);
-                double currTimeToSrc = timeToSrc(curr, src);
-                if (currTimeToSrc < bestTimeToSrc) {
-                    bestElevIndex = i;
-                    bestTimeToSrc = currTimeToSrc;
-                }
-            }
-        }
-        return bestElevIndex;
-    }
+
 }
